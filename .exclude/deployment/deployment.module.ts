e645fa84bc971d50e040196a2a96a41b7976e845
Marcus S. Abildskov <esetmarcussharkgamingcomputer@gmail.com>
2018-10-31 15:10:14 +0100

    chore: remove unnecessary files

--- .exclude/deployment/deployment.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { DeploymentController } from './deployment.controller';
-import { CommonModule } from '../common.module';
-
-@Module({
-  imports: [CommonModule],
-  controllers: [DeploymentController]
-})
-export class DeploymentModule {}
--- .exclude/namespace/exceptions/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './fetch-namespace.exception';
-export * from './create-namespace.exception';
